6abcd08d454936abebfd186916e5c90b33ccca1d
Frank <419610242@qq.com>
2017-05-25 18:55:40 +0800

    提交测试

--- ServersCluster/Program.cs
+++ ServersCluster/Program.cs
@@ -6,11 +6,11 @@ namespace ServersCluster
     {
         static void Main(string[] args)
         {
+            //提交测试
             Console.WriteLine("-------------");
             float x = (float)0.49;
             Console.WriteLine("-----{0}-----",x);
             Console.WriteLine("-------------");
-            //
         }
     }
 }
