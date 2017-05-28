0e6e39e6b50688e71b5e65d8b5be4b912bf24129
FRee22858 <419610242@qq.com>
2017-05-29 02:01:29 +0800

    vs2017 test

--- ServersCluster/Program.cs
+++ ServersCluster/Program.cs
@@ -6,7 +6,7 @@ namespace ServersCluster
     {
         static void Main(string[] args)
         {
-            //提交测试
+            //提交测试vs2017
             Console.WriteLine("-------------");
             float x = (float)0.49;
             Console.WriteLine("-----{0}-----",x);
